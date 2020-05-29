f9fd56a4d8bd9d368acc35e9d276ffe6090a18cf
Stepa K <stepan.karpov@list.ru>
2020-05-29 14:29:56 +0300

    bot editer

--- telegram_bot.py
+++ telegram_bot.py
@@ -10,8 +10,8 @@ def make_start(bot, update):
 	print("first message recieved")
 	bot.send_message(chat_id=update.message.chat_id, text="Hello. My Name is Isinka. I am robot AI. I was build to manage robot with a help of speech so you can find some mistakes in my request but they are all great if you'll try to pronounce them.")
 
-def do_echo(bot, update):
-	text = update.message.text.lower()
+def answer(bot, update):
+	text = update.message.text.lower().replace(".", "").replace("?", "").replace("!", "").strip(" ")
 	print("Request: " + str(text))
 	answer = prepare_answer(text).lower()
 	ready_answer = ""
@@ -28,7 +28,7 @@ def main():
 	updater = Updater(bot=bot)
 
 	start_handler = CommandHandler("start", make_start)
-	message_handler = MessageHandler(Filters.text, do_echo)
+	message_handler = MessageHandler(Filters.text, answer)
 
 	updater.dispatcher.add_handler(start_handler)
 	updater.dispatcher.add_handler(message_handler)
